be86077cf6cb2df856facc8eb15b072eb441ce89
Artem Shibaev <2fasti.miem@gmail.com>
2020-07-12 16:41:20 +0300

    Fix bug in displaing ECG when sampling is different than 1000 Hz.

---
+++ static/js/ecg_graphs.js
@@ -0,0 +1,120 @@
+function drawLeads(params, data) {
+
+    let NUM_LEADS = 12
+
+    data = data.data
+    const myNode = document.getElementById("graph");
+    while (myNode.lastElementChild) {
+        myNode.removeChild(myNode.lastElementChild);
+    }
+
+    for (let i = 0; i < NUM_LEADS; i++) {
+        var div = document.createElement('div')
+        div.className = "chart"
+        div.id = "chartContainer" + i
+        div.style = "position: relative; width: 100%; height: 200px;display: inline-block;"
+        document.getElementById('graph').appendChild(div)
+        drawSingleLead(i, 'chartContainer' + i, data[i])
+    }
+
+}
+
+function drawSingleLead(lead, containerName, singleLeadData) {
+    let leadNames = {
+        0: "I",
+        1: "II",
+        2: "III",
+        3: "aVR",
+        4: "aVL",
+        5: "aVF",
+        6: "V1",
+        7: "V2",
+        8: "V3",
+        9: "V4",
+        10: "V5",
+        11: "V6"
+    }
+
+    var xAxisStripLinesArray = [];
+    var yAxisStripLinesArray = [];
+    var dps = [];
+    var dataPointsArray = singleLeadData;
+
+    var chart = new CanvasJS.Chart(containerName, {
+        height: 200,
+        zoomEnabled: true,
+        animationEnabled: true,
+        animationDuration: 2000,
+        showInLegend: true,
+        // title: {
+        //     text: lead,
+        // },
+        subtitles: [{
+            fontSize:15,
+            text: "Lead " + leadNames[lead] + "      25мм/сек   10мм/мв",
+            horizontalAlign: "left",
+        }],
+        axisY: {
+            stripLines: yAxisStripLinesArray,
+            gridThickness: 0,
+            gridColor: "#DC74A5",
+            // lineColor: "#DC74A5",
+            // tickColor: "#DC74A5",
+            labelFontColor: "#DC74A5",
+        },
+        axisX: {
+            stripLines: xAxisStripLinesArray,
+            // gridThickness: 2,
+            gridColor: "#DC74A5",
+            lineColor: "#DC74A5",
+            // tickColor: "#DC74A5",
+            labelFontColor: "#DC74A5",
+        },
+        data: [{
+            type: "spline",
+            color: "black",
+            dataPoints: dps
+        }]
+    });
+
+    addDataPointsAndStripLines();
+    chart.render();
+
+    function addDataPointsAndStripLines() {
+        //dataPoints
+        for (var i = 0; i < dataPointsArray.length; i++) {
+            dps.push({
+                y: dataPointsArray[i]
+            });
+        }
+        //StripLines
+        for (var i = i=-4; i < singleLeadData.length; i = i + 1) {
+            yAxisStripLinesArray.push({
+            value: i,
+            thickness: 0.7,
+            color: "#DC74A5"
+        });
+        }
+        for (var i = 0; i < singleLeadData.length; i += 40) {
+            if (i % 200 !== 0) {
+                xAxisStripLinesArray.push({
+                value: i,
+                thickness: 0.3,
+                color: "#DC74A5"
+            });
+            } else if ( i % 1000 == 0) {
+                xAxisStripLinesArray.push({
+                value: i,
+                thickness: 1.5,
+                color: "#DC74A5"
+                });
+            } else {
+                xAxisStripLinesArray.push({
+                value: i,
+                thickness: 0.7,
+                color: "#DC74A5"
+            });
+            }
+        }
+    }
+}
--- utils.py
+++ utils.py
@@ -1,9 +1,56 @@
 import wfdb
 from scipy.fftpack import rfft, irfft, fftfreq
+from scipy import interpolate
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def _resample_waveform(waveform, fs, new_fs):
+    """Resample training sample to set sample frequency."""
+
+    if len(waveform.shape) == 1:
+        waveform = waveform.reshape(-1, 1)
+    length, leads_num = waveform.shape
+
+    # Get time array
+    time = np.arange(length) * 1 / fs
+
+    # Generate new resampling time array
+    times_rs = np.arange(0, time[-1], 1 / new_fs)
+
+    interpolated_waves = np.zeros((len(times_rs), leads_num))
+
+    # Setup interpolation function for every leads
+    for lean_n in range(leads_num):
+        interp_func = interpolate.interp1d(x=time, y=waveform[:, lean_n], kind='linear')
+
+        # Interpolate contiguous segment
+        sample_rs = interp_func(times_rs)
+        interpolated_waves[:, lean_n] = sample_rs
+
+    return interpolated_waves
+
+
+# def _resample_waveform(self, waveform, fs):
+#     """Resample training sample to set sample frequency."""
+#     # Get time array
+#     time = np.arange(len(waveform)) * 1 / fs
+#
+#     # Generate new resampling time array
+#     times_rs = np.arange(0, time[-1], 1 / self.fs)
+#
+#     # Setup interpolation function
+#     interp_func = interpolate.interp1d(x=time, y=waveform, kind='linear')
+#
+#     # Interpolate contiguous segment
+#     sample_rs = interp_func(times_rs)
+#
+#     return sample_rs
 
 
 def read_mit_data(file):
+    NEW_FS = 1000
+
     def smooth_line(y, fd):
         # fd - частота дискретизации
         W = fftfreq(y.size, 1 / fd)
@@ -15,9 +62,12 @@ def read_mit_data(file):
         return cut_signal
 
     record = wfdb.rdrecord(file)
+    print(record.fs)
     # data = smooth_line(record.adc()[:, 0], record.fs)
-    data = record.adc()
-
+    data = record.adc().astype(np.float16)
+    if record.fs != NEW_FS:
+        data = _resample_waveform(data, fs=record.fs, new_fs=NEW_FS)
+    data /= 1000
     return data.transpose()
 
 
